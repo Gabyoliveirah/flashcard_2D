90696858b9de9478aba390c481077b489e13ea7b
Gabyoliveirah <161325857+Gabyoliveirah@users.noreply.github.com>
2024-10-10 07:56:35 -0300

    .

---
+++ app.js
@@ -0,0 +1,5 @@
+funtion criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementyById('container')
+    let cartao = document.createElement('article')
+    cartao.className = cartao
+}
---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'série',
+    'em que ano a série elite foi lançada?',
+    '2018'
+)
+
+criaCartao(
+'jogo',
+'em que ano o logo Brawl Stars foi criado?',
+'2017',
+)
+
+criaCartao(
+    
+)
